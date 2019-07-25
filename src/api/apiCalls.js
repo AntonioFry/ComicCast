ef9a3e6bbb79342fbdb68185ea8759498748189d
Antonio Fry <antoniofry2000@gmail.com>
2019-07-25 14:46:37 -0600

    add function that fetches based off search

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -7,3 +7,13 @@ export const getCharacter = async (id) => {
     throw new Error('Failed to fetch character');
   }
 }
+
+export const getSearchResults = async (search) => {
+  try {
+    const response = await fetch(`https://www.superheroapi.com/api.php/1983143645120471/search/${search}`);
+    const result = await response.json();
+    return result.results; 
+  } catch (error) {
+    throw new Error('Either there were no matching results, or failed to fetch');
+  }
+}
