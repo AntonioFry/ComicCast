205f2bb4b7af1d30bb288702552e9c20a916cda3
Antonio Fry <antoniofry2000@gmail.com>
2019-07-25 12:45:35 -0600

    created fetch that returns character by id

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,7 +1,9 @@
-export const getFeaturedCharacter = () => {
-
-}
-
-export const getSuggestedCharacters = () => {
-  
+export const getCharacter = async (id) => {
+  try { 
+    const response = await fetch(`https://www.superheroapi.com/api.php/1983143645120471/${id}`);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw new Error('Failed to fetch character');
+  }
 }
