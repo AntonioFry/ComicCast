f119db5980c3406aa96fb08533b9f5daefb2f4fa
Antonio Fry <antoniofry2000@gmail.com>
2019-07-25 13:29:41 -0600

    create Search component as class

---
+++ src/components/NavBar/NavBar.test.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+    }
+  }
+
+  handleChange = (e) => {
+
+  }
+
+  handleSubmit = (e) => {
+
+  } 
+
+  render() {
+    return (
+      <form>
+        <input
+        className="character-search-input"
+        placeholder="Search for a character"
+        value={this.state.search}
+        name="search"
+        />
+      </form>
+    )
+  }
+}
