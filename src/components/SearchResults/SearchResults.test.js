361cf36c09c272d4297f12a00c70241a80f61d4b
Antonio Fry <antoniofry2000@gmail.com>
2019-07-29 13:23:44 -0600

    pass SearchResults test

--- src/components/SearchResults/SearchResults.test.js
+++ src/components/SearchResults/SearchResults.test.js
@@ -8,14 +8,14 @@ describe('SearchResults', () => {
 
   beforeEach(() => {
     props = {
-      allCharcaters: [
+      allCharacters: [
         { name: 'guy', id: 1, image: 'image.jpg', key: 1 },
         { name: 'peep', id: 2, image: 'image.jpg', key: 2 },
         { name: 'gal', id: 3, image: 'image.jpg', key: 3 },
         { name: 'dude', id: 4, image: 'image.jpg', key: 4 }
       ]
     } 
-    wrapper = shallow(<SearchResults allCharcaters={props.allCharcaters} search=""/>)
+    wrapper = shallow(<SearchResults allCharacters={props.allCharacters} search=""/>)
   });
 
   it('should match the snapshot', () => {
