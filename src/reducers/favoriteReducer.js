0082f53f5c62d917684f4929c7d97e649a3fab37
Antonio Fry <antoniofry2000@gmail.com>
2019-07-30 12:49:28 -0600

    edit styling on links in cards

--- src/reducers/favoriteReducer.js
+++ src/reducers/favoriteReducer.js
@@ -2,6 +2,9 @@ export const favoriteReducer = (state = [], action) => {
   switch (action.type) {
     case "FAVORITE_CHARACTER":
       return [...state, action.characterId];
+
+    // case "UNFAVORITE_CHARACTER":
+      
   
     default:
       return state;
