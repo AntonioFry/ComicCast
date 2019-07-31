3479a55eab15fd4f831a039f6b25a3bb24ff8ce5
Antonio Fry <antoniofry2000@gmail.com>
2019-07-31 15:44:17 -0600

    add cleaner

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,10 +1,13 @@
+import { cleaner } from './cleaner';
+
 export const getCharacters = async () => {
   try { 
     const response = await fetch(`https://api.sheety.co/50e47574-a7cb-4e95-8550-1975896a4e3e`);
     console.log(response)
     const result = await response.json();
-    console.log(result);
-    return result;
+    const cleanedResult = await cleaner(result);
+    console.log(cleanedResult);
+    return cleanedResult;
   } catch (error) {
     throw new Error('Failed to fetch characters');
   }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,10 +5,17 @@ import PropTypes from 'prop-types';
 import { setSearchResult } from '../../actions';
 import { connect } from 'react-redux';
 
-export const Card = ({ name, id, image, setSearchResult }) => {
+export const Card = ({ name, id, image, setSearchResult, favorite }) => {
+  let bookmarkImg;
+  if (favorite === true) {
+    bookmarkImg = '../../images/bookmark-black.svg';
+  } else {
+    bookmarkImg = '../../images/bookmark-white.svg';
+  }
   return (
     <Link className="card-link" to={`/${id}`} onClick={() => setSearchResult("")}>
       <div className="character-card" style={{backgroundImage: `url(${image})`}}>
+        <img className="bookmark-img" src={require(bookmarkImg)} alt='bookmark icon'/>
         <div className="character-card-name-box">
           <h3 className="character-card-text">{name}</h3>
         </div>
