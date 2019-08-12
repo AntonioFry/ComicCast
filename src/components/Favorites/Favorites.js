8497572468d1a8095e1fbd8fc549d877b72af575
Antonio Fry <antoniofry2000@gmail.com>
2019-08-11 18:39:55 -0600

    renders facorite characters in favorites

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Card from '../Card/Card';
 import './Favorites.css';
 
-export const Favorites = () => {
+export const Favorites = ({ allCharacters }) => {
+  const favorited = allCharacters.filter(charcater => charcater.favorite === true);
+  const cards = favorited.map(character => {
+    return (
+      <Card
+        name={character.name}
+        id={character.id}
+        image={character.image}
+        key={character.id}
+        favorite={character.favorite}
+      />
+    )
+  });
   return (
     <section>
-       
+      {cards}
     </section>
   )
 }
 
-export default Favorites;
+const mapStateToProps = (store) => ({
+  allCharacters: store.allCharacters
+});
+
+export default connect(mapStateToProps)(Favorites);
